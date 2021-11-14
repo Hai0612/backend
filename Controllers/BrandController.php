168616be9e1f1ef525a955352b9728f20e0d64b4
Hai0612 <hai06122001@gmail.com>
2021-11-14 18:18:58 +0700

    Revert "15h47 14/11"
    This reverts commit 4fffaab666c3ebf891b19068090ef19bf4e1727f.

--- Controllers/BrandController.php
+++ Controllers/BrandController.php
@@ -16,14 +16,14 @@ class BrandController extends BaseController
         if (isset($_GET['page'])) {
             $page  = $_GET['page'];
         }
-        $brand_id = NULL;
-        if (isset($_GET['brand_id'])) {
-            $brand_id = $_GET['brand_id'];
+        $category_id = NULL;
+        if (isset($_GET['category_id'])) {
+            $category_id = $_GET['category_id'];
         }
-        $brand_id = str_replace('-', ' ', $brand_id);
-        $products = $this->brandModel->showbyBrandId(BrandModel::TABLE, $brand_id, $page);
+        $category_id = str_replace('-', ' ', $category_id);
+        $products = $this->categoryModel->showbyCategoryId(CategoryModel::TABLE, $category_id, $page);
         $nextPage = (sizeof($products) === 9) ? ($page + 1) : $page;
-        $this->view('frontend.brand.index', [
+        $this->view('frontend.categories.index', [
             'products' => $products,
             'nextPage' => $nextPage,
         ]);
@@ -40,82 +40,5 @@ class BrandController extends BaseController
         );
     }
     
-    public function addbrand()
-    {   
-        // $_POST['id_'] = 10;
-        // $_POST['name_brand'] = " brand";
-        // $_POST['desc_brand'] = "new desc";
-        // $_POST['image'] = "new image"; 
-        if (isset($_POST['id_']) && isset($_POST['name_brand']) && isset($_POST['desc_brand']) && isset($_POST['image'])) {
-            $id_ = $_POST['id_'];
-            $name_brand = $_POST['name_brand'];
-            $desc_brand = $_POST['desc_brand'];
-            $image = $_POST['image'];
-            $flag = $this->brandModel->insertBrand(BrandModel::TABLE, $id_, $name_brand, $desc_brand, $image);
-        }
-        if ($flag) {
-            echo json_encode(
-                [
-                    'status' => 200,
-                    'payload' => $flag
-                ]
-            );
-        } else {
-            echo json_encode(
-                [
-                    'status' => 404,
-                ]
-            );
-        }
-    }
-
-    public function editBrand()
-    {   
-        // $_POST['id_'] = 10;
-        // $_POST['name_brand'] = "new name";
-        // $_POST['desc_brand'] = "newer desc";
-        // $_POST['image'] = "newer image";
-        if (isset($_POST['id_']) && isset($_POST['name_brand']) && isset($_POST['desc_brand']) && isset($_POST['image'])) {
-            $id_ = $_POST['id_'];
-            $name_brand = $_POST['name_brand'];
-            $desc_brand = $_POST['desc_brand'];
-            $image = $_POST['image'];
-            $flag = $this->brandModel->editBrandById(BrandModel::TABLE, $id_, $name_brand, $desc_brand, $image);
-            if ($flag) {
-                echo json_encode(
-                    array(
-                        'status' => 200,
-                        'payload' => $flag,
-                    )
-                );
-            } else {
-                echo json_encode(
-                    [
-                        'status' => 404,
-                    ]
-                );
-            }
-        }
-    }
-
-
-    public function deleteBrand()
-    {   
-        // $_POST['id_'] = 10;
-        if (isset($_POST['id_'])) {
-            $flag = $this->brandModel->deleteById(BrandModel::TABLE, $_POST['id_']);
-            if ($flag) {
-                echo json_encode([
-                    'status' => 200,
-                    'message' => 'ok'
-                ]);
-            } else {
-                echo json_encode([
-                    'status' => 400,
-                    'message' => 'bad request'
-                ]);
-            }
-            exit;
-        }
-    }
+   
 }
